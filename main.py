9ad89027c8e47ed55f4b04d667e633587331d2dd
Austin Saugrich <saugrich@gmail.com>
2023-10-14 02:17:41 -0500

    clean data set

---
+++ main.py
@@ -0,0 +1,16 @@
+from champion import Champion
+import json
+import sys
+# champ = Champion()
+# print(champ.name)
+
+champions = []
+
+def isMana(mp):
+    if mp:
+        return True
+    else:
+        return False
+
+
+
