8d766e2ae1cb41ebbc3450b1905d65aaf999b27f
Austin Saugrich <saugrich@gmail.com>
2023-10-15 13:35:53 -0500

    added random champ selector

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from champion import Champion
 import json
 import sys
+import random
 # champ = Champion()
 # print(champ.name)
 
 champions = []
 
-def isMana(mp):
-    if mp:
-        return True
-    else:
-        return False
 
 
+def random_champ():
+    champ_count = len(champions)
+    return random.randint(1, champ_count) - 1
+    
 
+def load_champ():
+    with open('./leaguelist.json') as champs_file:
+        data = json.load(champs_file)
+        for champs in data:
+             champions.append(champs)
+
+    
+load_champ()
+id = random_champ()
+print(champions[id])
